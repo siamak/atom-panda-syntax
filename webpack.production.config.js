368719f2a1122d9f8babf6c26c430359bc525064
Siamak Mokhtari <s.mokhtari75@gmail.com>
2016-08-03 12:04:00 +0430

    Responsive Design

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -53,11 +53,16 @@ module.exports = {
 
 	plugins: [
 		new webpack.optimize.DedupePlugin(),
+		new webpack.DefinePlugin({
+			"process.env": {
+				NODE_ENV: JSON.stringify("production")
+			}
+		}),
 		new uglifyJsPlugin({
 			compress: {
 				warnings: false
 			}
-		}),
+		})
 	],
 
 
